024a4ac671da30dd6c95aae148fc30cc9208cf52
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-26 00:31:09 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,5 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+<<<<<<< HEAD
     def change
       create_table :artists do |t|
         t.string :name
@@ -8,3 +9,14 @@ class CreateArtists < ActiveRecord::Migration[5.2]
       end
     end
   end
+=======
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
+>>>>>>> 06e42834c7306775d9f943686df2b1c8d35b6d17
